8f0e3cdb4a400e3d12e0e59c563ea6116f139a60
IT26101761 <it26101761@my.sliit.lk>
2026-06-14 19:58:37 +0530

    Add IT26101761Lab2Q3.java to Q3

---
+++ Q3/IT26101761Lab2Q3.java
@@ -0,0 +1,8 @@
+public class IT26101761Lab2Q3 {
+	public static void main (String [] args) {
+		double A = 3;
+		double B = 4;
+		double Hypotenuse = Math.sqrt((A*A) + (B*B));
+		System.out.println("Length of the hypotenuse: " + Hypotenuse);
+	}
+}
