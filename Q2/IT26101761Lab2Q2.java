f3310c8d34302bb6dc189b47b81925e8fdbfbb0e
IT26101761 <it26101761@my.sliit.lk>
2026-06-14 19:57:52 +0530

    Add IT26101761Lab2Q2.java to Q2

---
+++ Q2/IT26101761Lab2Q2.java
@@ -0,0 +1,10 @@
+public class IT26101761Lab2Q2 {
+	public static void main (String [] args) {
+		double len = 10;
+		double PerimeterSquare = 4 * len;
+		double Circumference = PerimeterSquare;
+		double PI = 3.14;
+		double Radius = Circumference / (2 * PI);
+		System.out.println("Radius of the circular fence: " + Radius);
+	}
+}
