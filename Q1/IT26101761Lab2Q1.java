d4fd20f3c05c560ee0c8cde29612a479664194eb
IT26101761 <it26101761@my.sliit.lk>
2026-06-14 19:57:16 +0530

    Add IT26101761Lab2Q1.java to Q1

---
+++ Q1/IT26101761Lab2Q1.java
@@ -0,0 +1,9 @@
+public class IT26101761Lab2Q1 {
+	public static void main(String [] args) {
+		double perimeter = 100;
+		double len = 2 * perimeter / 7;
+		double wid = 3.0/4 * len;
+		System.out.println("Length of the fence: " + len);
+		System.out.println("Width of the fence: " + wid);
+	}
+}
